ddc390830c990e6f0989875b44e7c6765e0d495a
박진아 <jin1090@greencatsoft.com>
2025-08-05 14:26:31 +0900

    전화번호와 이메일 중복 체크를 위한 HEAD 메서드 엔드포인트 추가

--- src/main/java/com/spring/skeleton/repository/CompanyRepository.java
+++ src/main/java/com/spring/skeleton/repository/CompanyRepository.java
@@ -3,9 +3,13 @@ package com.spring.skeleton.repository;
 import com.spring.skeleton.entity.CompanyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Collection;
 import java.util.Optional;
 
 public interface CompanyRepository extends JpaRepository<CompanyEntity, Long> {
     Optional<CompanyEntity> findByEmail(String email);
+
     Optional<CompanyEntity> findByPhoneOrEmail(String phone, String email);
+
+    Collection<Object> findByPhone(String phone);
 }
