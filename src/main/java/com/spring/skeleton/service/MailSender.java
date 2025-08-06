7fcd0aa297f79323fae37603457c5ea9ec2406c7
박진아 <jin1090@greencatsoft.com>
2025-08-06 14:51:55 +0900

    메일 환경 설정 및 메일 전송 서비스 추가

---
+++ src/main/java/com/spring/skeleton/service/MailSender.java
@@ -0,0 +1,27 @@
+package com.spring.skeleton.service;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailSender {
+    private final JavaMailSender mailSender;
+
+    @Autowired
+    public MailSender(JavaMailSender mailSender) {
+        this.mailSender = mailSender;
+    }
+
+    public void send(String to, String subject, String htmlContent) throws MessagingException {
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+        helper.setTo(to);
+        helper.setSubject(subject);
+        helper.setText(htmlContent, true);
+        mailSender.send(message);
+    }
+}
